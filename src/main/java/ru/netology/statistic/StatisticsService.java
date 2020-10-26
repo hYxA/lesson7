68bd04afa586cb08f1e81f5c39035138efb54879
hYxA <zybu@live.ru>
2020-10-26 10:13:57 +0300

    update to StatisticsService

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -1,20 +1,130 @@
 package ru.netology.statistic;
 
-public class  StatisticsService {
+public class StatisticsService {
+    int[] deals = new int[12]; // = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+    int expectedSumm;
+    long average;
+    int belowAverage = 0;
+    int aboveAverage = 0;
+
     /**
-     * Calculate index of max income
-     *
-     * @param incomes — array of incomes
-     * @return — index of first max value
+     * данные по месяцам
+     int month1 = 8;
+     int month2 = 15;
+     int month3 = 13;
+     int month4 = 15;
+     int month5 = 17;
+     int month6 = 20;
+     int month7 = 19;
+     int month8 = 20;
+     int month9 = 7;
+     int month10 = 14;
+     int month11 = 14;
+     int month12 = 18;
      */
-    public long findMax(long[] incomes) {
-        long current_max_index = 0;
-        long current_max = incomes[0];
-        for (long income : incomes)
-            if (current_max < income)
-                current_max = income;
-        return current_max;
+
+    /**
+     * перенос данных в массив для удобной обработки и расчётов
+     public int[] dataTranferToArray(int month1, int month2, int month3, int month4, int month5, int month6, int month7, int month8, int month9, int month10, int month11, int month12, int[] deals) {
+     deals[0] = month1;
+     deals[1] = month2;
+     deals[2] = month3;
+     deals[3] = month4;
+     deals[4] = month5;
+     deals[5] = month6;
+     deals[6] = month7;
+     deals[7] = month8;
+     deals[8] = month9;
+     deals[9] = month10;
+     deals[10] = month11;
+     deals[11] = month12;
+     return deals;
+     }
+     */
+
+
+    /**
+     * расчёт суммы продаж
+     */
+    public int sumCalculate(int[] deals, int expectedSumm) {
+        int summ = 0;
+        for (int deal : deals) {
+            summ += deal;
+        }
+
+        return summ;
+    }
+
+    /**
+     * расчёт среднего числа продаж в месяц
+     */
+    public long averageCalculate(int summ, int[] deals) { //, long expectedAverage
+        long average = summ / deals.length;
+        return average;
     }
 
 
+    // забыл, как вернуть номер "такого-то" элемента массива, поэтому немного некрасиво
+
+    /**
+     * последний месяц с наивысшими продажами
+     */
+    public int maxDealsIndex(int[] deals) {
+        int index = 1;
+        int maxDealsIndex = 1;
+        int maxDeal = 0;
+        for (int deal : deals) {
+            if (maxDeal <= deal) {
+                maxDeal = deal;
+                maxDealsIndex = index;
+            }
+            index += 1;
+        }
+
+        return maxDealsIndex;
+    }
+
+    /**
+     * последний месяц с наихудшими продажами
+     */
+    public int minDealsIndex(int[] deals) {
+        int index = 1;
+        int minDealsIndex = 1;
+        int minDeal = deals[1];
+        for (int deal : deals) {
+            if (minDeal >= deal) {
+                minDeal = deal;
+                minDealsIndex = index;
+            }
+            index += 1;
+        }
+
+        return minDealsIndex;
+    }
+
+    /**
+     * количество месяцев с количеством продаж ниже среднего
+     */
+    public int belowAverage(int[] deals, long average) {
+        int belowAverage = 0;
+        for (int deal : deals) {
+            if (deal < average) {
+                belowAverage += 1;
+            }
+        }
+        return belowAverage;
+    }
+
+    /**
+     * количество месяцев с количеством продаж выше среднего
+     */
+    public int aboveAverage(int[] deals, long average) {
+        int aboveAverage = 0;
+        for (int deal : deals) {
+            if (deal < average) {
+                belowAverage += 1;
+            }
+        }
+        return belowAverage;
+    }
 }
--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -2,18 +2,73 @@ package ru.netology.statistic;
 
 import org.junit.jupiter.api.Test;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticsServiceTest {
+    int expectedSumm = 180;
+    long expectedAverage = 15;
+
+    //int[] deals = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+    StatisticsService StatisticService = new StatisticsService();
 
     @Test
-    void findMax() {
-        StatisticsService service = new StatisticsService();
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
-        long actual = service.findMax(incomesInBillions);
+    void calculateSumm() {
 
-        assertEquals(expected, actual);
+        int[] deals = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int summ = StatisticService.sumCalculate(deals, expectedSumm);
+        assertEquals(expectedSumm, summ);
+
     }
 
+    @Test
+    void calculateAverage() {
+        int summ = 180;
+        int[] deals = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long average = StatisticService.averageCalculate(summ, deals);
+        assertEquals(expectedAverage, average);
+
+    }
+
+    @Test
+    void calculateMaxDealsIndex() {
+
+        int[] deals = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expectedMaxDealsIndex = 8;
+        int maxDealsIndex = StatisticService.maxDealsIndex(deals);
+        assertEquals(expectedMaxDealsIndex, maxDealsIndex);
+
+    }
+
+    @Test
+    void calculateMinDealsIndex() {
+
+        int[] deals = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expectedMinDealsIndex = 9;
+        int minDealsIndex = StatisticService.minDealsIndex(deals);
+        assertEquals(expectedMinDealsIndex, minDealsIndex);
+
+    }
+
+    @Test
+    void calculateBelowAverage() {
+
+        int[] deals = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long average = 15;
+        int expectedBelowAverage = 5;
+        int belowAverage = StatisticService.belowAverage(deals, average);
+        assertEquals(expectedBelowAverage, belowAverage);
+
+    }
+
+    @Test
+    void calculateAboveAverage() {
+
+        int[] deals = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long average = 15;
+        int expectedAboveAverage = 5;
+        int aboveAverage = StatisticService.aboveAverage(deals, average);
+        assertEquals(expectedAboveAverage, aboveAverage);
+
+    }
 }
